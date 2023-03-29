1ac5c4dfdd3e71abdd29b0b989c3531767d330d3
StaticBits <puppy2655@gmail.com>
2023-03-29 19:24:48 -0400

    Proper movement code

--- src/systems/moveSystem.cpp
+++ src/systems/moveSystem.cpp
@@ -12,14 +12,37 @@ namespace Systems
 	void updateVelocity(entt::registry& registry, Window& window)
 	{
 		auto view = registry.view<Comps::Movement, Tags::KeyboardInput>();
-		
+
 		for (const entt::entity entity : view)
 		{
 			auto& movement = view.get<Comps::Movement>(entity);
 
-			//// Updates velocity with acceleration based on input
-			movement.vel.x += (window.getKeyState(SDLK_RIGHT) - window.getKeyState(SDLK_LEFT)) * movement.acceleration * window.getDeltaTime();
-			movement.vel.y += (window.getKeyState(SDLK_DOWN) - window.getKeyState(SDLK_UP)) * movement.acceleration * window.getDeltaTime();
+			const int dirX = (window.getKeyState(SDLK_RIGHT) - window.getKeyState(SDLK_LEFT));
+			const int dirY = (window.getKeyState(SDLK_DOWN) - window.getKeyState(SDLK_UP));
+
+			if (dirX != 0)
+				movement.vel.x += dirX * movement.acceleration * window.getDeltaTime(); // Accelerate
+			else // Move towards zero
+				movement.vel.x += (movement.vel.x < 0 ? 1 : -1) * movement.acceleration * window.getDeltaTime();
+
+			// Lock between max speed
+			if (movement.vel.x > movement.maxSpeed)        movement.vel.x = movement.maxSpeed;
+			else if (movement.vel.x < -movement.maxSpeed)  movement.vel.x = -movement.maxSpeed;
+
+			else if (dirX == 0 && movement.vel.x <= movement.acceleration * window.getDeltaTime() && movement.vel.x >= -movement.acceleration * window.getDeltaTime())
+				movement.vel.x = 0.0f; // Near enough to zero to round to zero
+
+			// same for y direction
+			if (dirY != 0)
+				movement.vel.y += dirY * movement.acceleration * window.getDeltaTime();
+			else
+				movement.vel.y += (movement.vel.y < 0 ? 1 : -1) * movement.acceleration * window.getDeltaTime();
+
+			if (movement.vel.y > movement.maxSpeed)        movement.vel.y = movement.maxSpeed;
+			else if (movement.vel.y < -movement.maxSpeed)  movement.vel.y = -movement.maxSpeed;
+
+			else if (dirY == 0 && movement.vel.y <= movement.acceleration * window.getDeltaTime() && movement.vel.y >= -movement.acceleration * window.getDeltaTime())
+				movement.vel.y = 0.0f;
 		}
 	}
 
